0ad2d6c905dfd06fb5d18ab14a13753d7f094c3e
lukasz <gdx182306@student.gdansk.merito.pl>
2026-04-09 12:22:47 +0200

    add faktury tab

--- Faktury/MainWindow.xaml.cs
+++ Faktury/MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Faktury
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // after adding/removing item to ObservableCollection UI should update automatically
+        public ObservableCollection<InvoiceItem> CurrentInvoiceItems { get; set; }
         public MainWindow()
         {
-            
+            InitializeComponent();
+            CurrentInvoiceItems = new ObservableCollection<InvoiceItem>();
+            Invoice_DataGrid.ItemsSource = CurrentInvoiceItems;
         }
     }
+
+    public class InvoiceItem
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public double Ilosc { get; set; }
+        public string JednostkaMiary { get; set; }
+        public decimal CenaNetto { get; set; } // TODO: Stawka podatku / obliczanie automatycznie ceny netto na podstawie brutto i na odwrot
+        public decimal CenaBrutto { get; set; }
+        public decimal Vat { get; set; }
+
+        // TODO: Liczenie wartosci na podrawie ilosc * cena
+        public decimal WartoscNetto { get; set; }
+        public decimal WartoscBrutto { get; set; }
+    }
+
 }
--- Faktury/obj/Debug/net8.0-windows/Faktury.AssemblyInfo.cs
+++ Faktury/obj/Debug/net8.0-windows/Faktury.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Faktury")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c23b746785090444a02c5998e410e9b5d5ece18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Faktury")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Faktury")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
